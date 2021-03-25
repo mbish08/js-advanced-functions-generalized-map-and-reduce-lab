ef97c2269f191242c277044c685d9ccc024187f1
mbish08 <mdrew8@gmail.com>
2021-03-25 11:50:28 -0700

    commit tree cd26616b625ae3e46b4621e3fc2d8440b7cb4201

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your functions here
+function map(src, callBack) {
+    let array = []
+    for (let i=0; i < src.length; i++) {
+        let element = src[i]
+        array.push(callBack(element))
+    }
+    return array
+}
+
+function reduce(src, callBack, startValue=0) {
+    let r = (!!startValue) ? startValue : src[0]
+    let i = (!!startValue) ? 0 : 1
+
+    for (; i < src.length; i++) {
+        r = callBack(src[i], r)
+    }
+  return r;
+}
